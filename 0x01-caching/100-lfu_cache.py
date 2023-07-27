296f9aff0b631d081b798ea3c0231e7e8c9cadcc
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2023-07-26 19:33:11 -0800

    Update 100

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 """ LFUCache module
+Create a class LFUCache that inherits from BaseCaching
+and is a caching system:
+
+You must use self.cache_data - dictionary from the parent
+class BaseCaching
+You can overload def __init__(self): but don’t forget to 
+call the parent init: super().__init__()
+
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the item
+value for the key key.
+If key or item is None, this method should not do anything.
+If the number of items in self.cache_data is higher that 
+BaseCaching.MAX_ITEMS:
+you must discard the least frequency used item (LFU algorithm)
+if you find more than 1 item to discard, you must use the LRU 
+algorithm to discard only the least recently used you must print 
+DISCARD: with the key discarded and following by a new line
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in self.cache_data, return None.
 """
 
 from collections import defaultdict
 from datetime import datetime
 
 BaseCaching = __import__('base_caching').BaseCaching
+
 
 class LFUCache(BaseCaching):
     """ LFUCache class inherits from BaseCaching and implements the LFU eviction policy.
@@ -26,7 +48,8 @@ class LFUCache(BaseCaching):
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             min_freq = min(self.frequency.values())
             lfu_items = [k for k, v in self.frequency.items() if v == min_freq]
-            lru_key = min(lfu_items, key=lambda k: self.cache_data[k].timestamp)
+            lru_key = min(
+                lfu_items, key=lambda k: self.cache_data[k].timestamp)
             print("DISCARD:", lru_key)
             del self.cache_data[lru_key]
             del self.frequency[lru_key]
@@ -43,6 +66,7 @@ class LFUCache(BaseCaching):
         self.frequency[key] += 1
         self.cache_data[key].timestamp = datetime.now()
         return self.cache_data[key].value
+
 
 class CacheItem:
     """ CacheItem class to store the item and its timestamp.
