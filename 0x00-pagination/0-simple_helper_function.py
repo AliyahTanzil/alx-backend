a59cf45220572cbc08bb39052d2b456d9db66364
AliyahTanzil <sesaymohamedaugustin@gmail.com>
2023-07-28 07:10:40 -0800

    update

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -38,6 +38,6 @@ def index_range(page, page_size):
     if page <= 0 or page_size <= 0:
         raise ValueError("Page and page_size must be positive integers.")
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
+    end_index = start_index + page_size
 
     return start_index, end_index
